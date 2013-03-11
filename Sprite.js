7d6f3b6997cd246ee229fa1c46f0556effd8f420
Muhammad Firdaus Zailani <mfirdaus@AsusEEE.local>
2013-03-11 23:36:29 +0800

    Refactor. More code

--- Sprite.js
+++ Sprite.js
@@ -1,5 +1,29 @@
 function Sprite(w,sw,sy,sfile){
-	function make_sprite(n,x,y,a){
+	/*
+	var anims=[];
+	var id=0;
+	function next_frames(called){
+		for(var i=0;i<anims.length;i++){
+			anims[i].next_frame();
+		}
+		id=setTimeout(next_frames,500)
+	}
+	next_frames();
+	*/
+	function make_sprite(n,x,y,a,delay){
+		if(typeof n=="object"){
+			var delay=delay|500
+			var curr=0,id=0;
+			var sprite=make_sprite(n[0],x,y,a)
+			sprite.next_frame=function(){
+				curr=(curr+1)%n.length;
+				sprite.changeSprite(n[curr])
+				id=setTimeout(sprite.next_frame,delay);
+			}
+			//anims.push(sprite);
+			sprite.next_frame();
+			return sprite;
+		}
 		x=x|0;
 		y=y|0;
 		var o=document.createElement("div");
@@ -13,14 +37,31 @@ function Sprite(w,sw,sy,sfile){
 			n=pn;
 			set_style();
 		}
-		o.move=function(px,py){
-			x+=px;
-			y+=py;
+		o.move=function(p,fn){
+			if(typeof fn=="function"&&!fn(x+p[0],y+p[1])){
+				return false;
+			}
+			x+=p[0];
+			y+=p[1];
 			set_style();
+			return true;
 		}
 
 		set_style();
 		return o;
 	}
 	return make_sprite;
+}
+
+function Map(sprite,map,map_width,terrain,map_obj){
+	for(var i=0;i<map.length;i++){
+		map_obj.appendChild(sprite(terrain[map[i]].tile,i%map_width,~~(i/map_width),0.5))
+	}
+	map_obj.check=function(x,y){
+			if((x>=0)&&(x<=map_width-1)&&(y<=map_width-1)&&(y>=0)){
+				return terrain[map[x+y*map_width]].walkable;
+			}
+			return false
+		}
+	return map_obj;
 }
